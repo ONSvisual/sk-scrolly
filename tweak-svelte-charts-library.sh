77932c23a092ecc308f602a3277e7fb2f9ad07fb
James Trimble <jtrim.ons@gmail.com>
2022-01-27 14:43:09 +0000

    Remove a line from tweaking script

--- tweak-svelte-charts-library.sh
+++ tweak-svelte-charts-library.sh
@@ -2,4 +2,4 @@
 
 sed -i.bak '1s/^import html2canvas.*$//' node_modules/@onsvisual/svelte-charts/src/js/utils.js
 
-sed -i.bak 's/width > 80/true/' node_modules/@onsvisual/svelte-charts/src/charts/*Chart.svelte
+# sed -i.bak 's/width > 80/true/' node_modules/@onsvisual/svelte-charts/src/charts/*Chart.svelte
